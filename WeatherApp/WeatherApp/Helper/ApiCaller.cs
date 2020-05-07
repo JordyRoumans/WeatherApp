b4922511639d06af4fb18f8a322799afdeed8b73
EPICdisast3r <43574933+EPICdisast3r@users.noreply.github.com>
2020-05-07 15:04:17 +0200

    API request voor loctia variabele

---
+++ WeatherApp/WeatherApp/Helper/ApiCaller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Helper
+{
+    public class ApiCaller
+    {
+        public static async Task<ApiResponse> Get(string url, string authId = null)
+        {
+            using (var client = new HttpClient())
+            {
+                // als authid niet bekend is passeer door
+                if (!string.IsNullOrWhiteSpace(authId))
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", authId);
+
+                var request = await client.GetAsync(url);
+                if (request.IsSuccessStatusCode)
+                {
+                    //succesvolle response
+                    return new ApiResponse { Response = await request.Content.ReadAsStringAsync() };
+                }
+                else
+                    //onsuccesvolle response, geef reden
+                    return new ApiResponse { ErrorMessage = request.ReasonPhrase };
+            }
+        }
+
+    }
+
+    public class ApiResponse
+    {
+        
+        // als errormessage == nul dan is de Api call succesvol
+        public bool Succesful => ErrorMessage == null;
+        public string ErrorMessage { get; set; }
+        public string Response { get; set; }
+    }
+}
--- WeatherApp/WeatherApp/Views/CurrentWeatherPage.xaml.cs
+++ WeatherApp/WeatherApp/Views/CurrentWeatherPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WeatherApp.Helper;
+using WeatherApp.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +17,53 @@ namespace WeatherApp.Views
         public CurrentWeatherPage()
         {
             InitializeComponent();
+            GetWeatherInfo();
+        }
+
+        private string Location = "Lanaken";
+
+        private async void GetWeatherInfo()
+        {
+            //Location wordt uit de string Location gehaald
+            var url = $"http://api.openweathermap.org/data/2.5/weather?q=" + Location + "&APPID=70c9d17ec1211e30b75ac292a72d685e&units=metric";
+
+            var result = await ApiCaller.Get(url);
+
+            if (result.Succesful)
+            {
+                try
+                {
+                    //converteer de JSON en steek het in result
+                    var weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(result.Response);
+
+                    //steek de weerinfo in de corresponderende vakken
+                    descriptionTxt.Text = weatherInfo.weather[0].description.ToUpper();
+                    iconImg.Source = $"w{weatherInfo.weather[0].icon}";
+                    cityTxt.Text = weatherInfo.name.ToUpper();
+                    temperatureTxt.Text = weatherInfo.main.temp.ToString("0");
+                    humidityTxt.Text = $"{weatherInfo.main.humidity}%";
+                    pressureTxt.Text = $"{weatherInfo.main.pressure} hpa";
+                    windTxt.Text = $"{weatherInfo.wind.speed} m/s";
+                    cloudinessTxt.Text = $"{weatherInfo.clouds.all}%";
+
+                    //converie van het UNIX UTC tijdsformaat naar algemeen datumformaat
+                    var dt = new DateTime().ToUniversalTime().AddSeconds(weatherInfo.dt);
+                    dateTxt.Text = dt.ToString("dddd, MMM dd").ToUpper();
+
+                }
+                catch (Exception ex)
+                {
+                    //errormessage bij exception en corresponderende message
+                    await DisplayAlert("Weather Info", ex.Message, "OK");
+
+                }
+
+            }
+            else
+            {
+                //indien er geen resultaat opgehaald kan worden wordt deze errormessage weergegeven
+                await DisplayAlert("Weather Info", "No weatherinformation found", "OK");
+            }
         }
     }
 }
